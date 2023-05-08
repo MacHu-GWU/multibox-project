6076b2d9bd71e93c8b0f880536e6e5ed0f829f83
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-05-08 14:10:39 -0400

    setup unit test

---
+++ tests/test_api.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+def test():
+    from multibox import api
+
+
+if __name__ == "__main__":
+    from hotkeynet.tests import run_cov_test
+
+    run_cov_test(__file__, "multibox.api", preview=False)
