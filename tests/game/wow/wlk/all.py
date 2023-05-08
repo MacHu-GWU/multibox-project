a9ea3e1ad5495eed1233091d0c7e5bb1efce23aa
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-05-08 14:54:02 -0400

    wow wlk talent

---
+++ tests/game/wow/wlk/all.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+if __name__ == "__main__":
+    from multibox.tests import run_cov_test
+
+    run_cov_test(__file__, "multibox.game.wow.wlk", preview=False, is_folder=True)
