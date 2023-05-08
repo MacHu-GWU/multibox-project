ce347e31612afcfd77deb520c16ddc275edcb5d8
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-05-08 14:56:51 -0400

    acc

--- tests/game/wow/test_game_wow_account.py
+++ tests/game/wow/test_game_wow_account.py
@@ -1,45 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import os
-from multibox.game.wow.model import (
+from pathlib_mate import Path
+from multibox.game.wow.account import (
     Account,
-    Character,
-    Talent,
-    TalentCategory,
-    Window,
+    AccountLoader,
 )
 
 
-class TestWindow:
+class TestAccount:
     def test_make(self):
-        window = Window.make(1)
-        assert window.label == "w01"
-        assert window.title == "WoW01"
+        loader = AccountLoader(path=Path.dir_here(__file__).joinpath("accounts.json"))
 
-
-class TestCharacter:
-    def test_set_method(self):
-        window = Window.make(3)
-        leader_1_window = Window.make(1)
-        leader_2_window = Window.make(2)
-
-        char1 = Character()
-        assert char1.is_leader_1 is False
-        assert char1.is_leader_2 is False
-
-        char2 = (
-            char1
-            .set_window(window)
-            .set_active()
-            .set_inactive()
-            .set_is_leader_1()
-            .set_not_leader_1()
-            .set_is_leader_2()
-            .set_not_leader_2()
-            .set_leader_1_window(leader_1_window)
-            .set_leader_2_window(leader_2_window)
-        )
-        assert char1 is char2
+        acc1 = loader.load("acc1")
+        assert isinstance(acc1, Account) is True
+        assert acc1.username == "acc1"
+        assert acc1.password == "pwd1"
 
 
 if __name__ == "__main__":
