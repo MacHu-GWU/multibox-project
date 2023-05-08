1926126e37a21d7d09f59cc69ad73a457117f291
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-05-08 16:31:08 -0400

    Create __init__.py

---
+++ multibox/game/wow/wlk/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from . import coordinator
+from .character import (
+    Character,
+    CharacterHelper,
+)
+from .talent import (
+    Talent,
+    TalentCategory,
+)
+from ..model import (
+    Window,
+    Account,
+)
