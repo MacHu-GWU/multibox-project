131926eb836d10ee2a7e8e9b851e9c822775949e
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-05-08 15:59:06 -0400

    game wow wlk coordinator

---
+++ tests/game/wow/wlk/test_game_wow_wlk_coordinator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from multibox.game.wow.wlk.coordinator import *
+
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    from multibox.tests import run_cov_test
+
+    run_cov_test(__file__, "multibox.game.wow.wlk.coordinator", preview=False)
