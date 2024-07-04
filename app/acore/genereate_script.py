38df2ff0ef445f705ee2a1cb5ff6ff20120b4c11
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-07-04 01:46:40 -0400

    made everything work

--- app/acore/genereate_script.py
+++ app/acore/genereate_script.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 
-from multibox.app.azerothcore_alliance_v2.api import (
-    dungeon_mode_fact,
-    # raid_mode_fact,
-)
+from multibox.app.acore.dataset import mode_fact
 
 # -----------------------------------------------------------------------------
 # Azerothcore - Alliance - Dungeon
 # -----------------------------------------------------------------------------
-mode = dungeon_mode_fact.x5p_alliance_r_abcde
+# mode = mode_fact.alliance_r_abcde_solo_dungeon
 
 # -----------------------------------------------------------------------------
 # Azerothcore - Alliance - Raid
 # -----------------------------------------------------------------------------
+# mode = mode_fact.alliance_r_abcdefghij_solo_raid
 # mode = raid_mode_fact.x10p_core_team
 # mode = raid_mode_fact.x10p_naxx_abomination_4th_boss
 # mode = raid_mode_fact.x25p_core_team
 # mode = raid_mode_fact.x25p_core_team_ICC_1
 
+# -----------------------------------------------------------------------------
+# Azerothcore - Horde - Dungeon
+# -----------------------------------------------------------------------------
+mode = mode_fact.horde_s_abcde_solo_dungeon
 
 # -----------------------------------------------------------------------------
 # Don't Touch
