2479978f9a4f1f31541174ea77688ed6069ff60a
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-07-05 12:25:29 -0400

    Update test_acore.py

--- tests/app/acore/test_acore.py
+++ tests/app/acore/test_acore.py
@@ -5,9 +5,11 @@ from multibox.app.acore.dataset import mode_fact
 
 def test():
     for mode in [
-        mode_fact.alliance_r_abcde_solo_dungeon,
-        # mode_fact.alliance_r_abcdefghij_solo_raid,
-        # mode_fact.horde_s_abcde_solo_dungeon,
+        mode_fact.acore_alliance_r_abcde_solo_dungeon,
+        mode_fact.acore_alliance_r_abcdefghij_solo_raid,
+        mode_fact.acore_horde_s_abcde_solo_dungeon,
+        mode_fact.warmane_quarterly_login_team1,
+        mode_fact.warmane_quarterly_login_team2,
     ]:
         mode.render(verbose=False)
 
