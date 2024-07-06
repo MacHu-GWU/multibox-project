75d97c7994962d231f645ae1e903232dce3daed9
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-07-05 23:07:01 -0400

    Update hk_group_12_special.py

--- multibox/game/wow/wlk/preset/my_mode/hk_group_12_special.py
+++ multibox/game/wow/wlk/preset/my_mode/hk_group_12_special.py
@@ -8,7 +8,7 @@ import typing as T
 import itertools
 
 from hotkeynet import api as hk
-from hotkeynet.api import KN
+from hotkeynet.api import KN, CAN
 
 from multibox.game.wow.wlk.api import TalentCategory as TC
 from ..my_act import api as act
@@ -23,28 +23,62 @@ class HotkeyGroup12SpecialMixin:
             id="Shift + Z",
             key=KN.SCROLOCK_ON(KN.SHIFT_(KN.Z)),
         ) as self.hk_shift_z:
+            lbs_shaman = self.lbs_by_tc(TC.shaman)
             with hk.SendLabel(
-                to=self.lbs_all,
+                to=list(lbs_shaman),
             ):
-                hk.Key.trigger()
+                act.Target.TARGET_RAID()
+                act.Shaman.Chain_Heal()
 
-        with hk.Hotkey(
-            id="Shift + T",
-            key=KN.SCROLOCK_ON(KN.SHIFT_(KN.T)),
-        ) as self.hk_shift_t:
+            lbs_paladin_holy = self.lbs_by_tc(TC.paladin_holy)
             with hk.SendLabel(
-                to=self.lbs_all,
+                to=list(lbs_paladin_holy),
             ):
-                hk.Key.trigger()
+                act.Target.TARGET_RAID()
+                act.PaladinHoly.Holy_Light()
 
-        with hk.Hotkey(
-            id="Shift + X",
-            key=KN.SCROLOCK_ON(KN.SHIFT_(KN.X)),
-        ) as self.hk_shift_x:
+            lbs_druid_resto = self.lbs_by_tc(TC.druid_resto)
             with hk.SendLabel(
-                to=self.lbs_all,
+                to=list(lbs_druid_resto),
             ):
-                hk.Key.trigger()
+                act.DruidRestoration.MB_HEAL_RAID()
+
+            lbs_priest_healer = self.lbs_by_tc(TC.priest_healer)
+            with hk.SendLabel(
+                to=list(lbs_priest_healer),
+            ):
+                act.PriestHoly.Prayer_of_Healing()
+
+            lbs_non_shaman_dps = self.lbs_by_tc(TC.dps).difference(lbs_shaman)
+            with hk.SendLabel(
+                to=list(lbs_non_shaman_dps),
+            ):
+                act.Target.TARGET_FOCUS_TARGET()
+                CAN.KEY_2()
+
+            lbs_tank = self.lbs_by_tc(TC.tank)
+            with hk.SendLabel(
+                to=list(lbs_tank),
+            ):
+                CAN.KEY_2()
+
+        # with hk.Hotkey(
+        #     id="Shift + T",
+        #     key=KN.SCROLOCK_ON(KN.SHIFT_(KN.T)),
+        # ) as self.hk_shift_t:
+        #     with hk.SendLabel(
+        #         to=self.lbs_all,
+        #     ):
+        #         hk.Key.trigger()
+        #
+        # with hk.Hotkey(
+        #     id="Shift + X",
+        #     key=KN.SCROLOCK_ON(KN.SHIFT_(KN.X)),
+        # ) as self.hk_shift_x:
+        #     with hk.SendLabel(
+        #         to=self.lbs_all,
+        #     ):
+        #         hk.Key.trigger()
 
     def build_hk_group_12_mixin(self: "Mode"):
         if "icc_boss1" in self.name:
