67eaa09e35fbb0086eb83c2e6d8318b02fb57027
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-07-12 17:20:08 -0400

    c

--- multibox/game/wow/wlk/preset/my_mode/hk_group_03_act_1_to_12/act6.py
+++ multibox/game/wow/wlk/preset/my_mode/hk_group_03_act_1_to_12/act6.py
@@ -6,6 +6,7 @@ todo: docstring
 
 import typing as T
 
+import hotkeynet.api as hk
 from hotkeynet.api import KN, CAN
 from multibox.game.wow.wlk.preset.my_mode.utils import TargetEnum
 
@@ -23,12 +24,16 @@ class Act6Mixin:
         """
         See :ref:`wow-wlk-act-1-to-10-tank-dps-healer`
         """
-        self.build_tank_default_action(key=KN.KEY_6)
-        self.build_dps_default_action(key=KN.KEY_6)
-        self.build_healer_default_action(
-            key=KN.KEY_6,
-            target=TargetEnum.TARGET_FOCUS_TARGET,
-        )
+        with hk.Hotkey(
+            id="Key6",
+            key=KN.SCROLOCK_ON(KN.KEY_6),
+        ) as self.hk_6:
+            self.build_tank_default_action(key=KN.KEY_6)
+            self.build_dps_default_action(key=KN.KEY_6)
+            self.build_healer_default_action(
+                key=KN.KEY_6,
+                target=TargetEnum.TARGET_FOCUS_TARGET,
+            )
 
     def build_act6(self: "Mode"):
         if self.name == "special_mode":
--- multibox/game/wow/wlk/preset/my_mode/hk_group_03_act_1_to_12/act7.py
+++ multibox/game/wow/wlk/preset/my_mode/hk_group_03_act_1_to_12/act7.py
@@ -6,6 +6,7 @@ todo: docstring
 
 import typing as T
 
+import hotkeynet.api as hk
 from hotkeynet.api import KN, CAN
 from multibox.game.wow.wlk.preset.my_mode.utils import TargetEnum
 
@@ -22,12 +23,16 @@ class Act7Mixin:
         """
         See :ref:`wow-wlk-act-1-to-10-tank-dps-healer`
         """
-        self.build_tank_default_action(key=KN.KEY_6)
-        self.build_dps_default_action(key=KN.KEY_6)
-        self.build_healer_default_action(
-            key=KN.KEY_6,
-            target=TargetEnum.TARGET_FOCUS_TARGET,
-        )
+        with hk.Hotkey(
+            id="Key7",
+            key=KN.SCROLOCK_ON(KN.KEY_7),
+        ) as self.hk_7:
+            self.build_tank_default_action(key=KN.KEY_7)
+            self.build_dps_default_action(key=KN.KEY_7)
+            self.build_healer_default_action(
+                key=KN.KEY_7,
+                target=TargetEnum.TARGET_FOCUS_TARGET,
+            )
 
     def build_act7(self: "Mode"):
         if self.name == "special_mode":
